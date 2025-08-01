240590a61c877069ac627405896e49a61beea66f
CastiSeek <castiseeka@gmail.com>
2025-08-01 19:54:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,23 @@
-const CACHE_NAME = 'gentle-alarm-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/Alarms/001.mp3',
-  '/Alarms/002.mp3',
-  '/icon-192.png',
-  '/icon-512.png'
-];
+importScripts('https://www.gstatic.com/firebasejs/9.6.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.0/firebase-messaging-compat.js');
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-  );
+// Тот же конфиг Firebase
+firebase.initializeApp({
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+const messaging = firebase.messaging();
 
-self.addEventListener('message', event => {
-  if (event.data.action === 'triggerAlarm') {
-    self.registration.showNotification('Wake Up!', {
-      body: 'Your alarm is going off!',
-      icon: 'icon-192.png',
-      vibrate: [200, 100, 200, 100, 200]
+// Обработчик фоновых уведомлений
+messaging.onBackgroundMessage(payload => {
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: 'icon.png',
+        vibrate: [200, 100, 200]
     });
-  }
 });
